837623feed23959098c38e3d55f7534dace7738e
Deepak K B <deepucsait@gmail.com>
2026-02-08 20:13:11 +0530

    max width

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 function downloadPage() {
     const element = document.getElementById('cv-main'); // whole page
     const options = {
-        margin: [5, 0, 5, 0],
+        margin: [10, 0, 10, 0],
         filename: 'deepak-kb_cv.pdf',
         image: { type: 'jpeg', quality: 0.98 },
-        html2canvas: { scale: 2 },
+        html2canvas: { scale: 3 },
         jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
     };
 
