f21d43924d28414cf6e36d359fe4c607007a1071
Deepak K B <deepucsait@gmail.com>
2026-02-08 19:34:12 +0530

    added export pdf

---
+++ index.js
@@ -0,0 +1,13 @@
+function downloadPage() {
+    const element = document.body; // whole page
+    const options = {
+        margin: 0.5,
+        filename: 'webpage.pdf',
+        image: { type: 'jpeg', quality: 0.98 },
+        html2canvas: { scale: 2 },
+        jsPDF: { unit: 'in', format: 'a4', orientation: 'portrait' }
+    };
+    console.log('Downloading page as PDF...', options);
+
+      html2pdf().set(options).from(element).save();
+}
