9d76a6f35975401d394b4a1fe07435e39a1d9c4e
Deepak K B <deepucsait@gmail.com>
2026-02-08 19:38:21 +0530

    Renamed pdf name

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function downloadPage() {
     const element = document.body; // whole page
     const options = {
         margin: 0.5,
-        filename: 'webpage.pdf',
+        filename: 'deepak-kb_cv.pdf',
         image: { type: 'jpeg', quality: 0.98 },
         html2canvas: { scale: 2 },
         jsPDF: { unit: 'in', format: 'a4', orientation: 'portrait' }
